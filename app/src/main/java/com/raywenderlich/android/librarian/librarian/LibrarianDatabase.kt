0aa5fda1b62d2e09efe601a05c9fb58681fed52c
Tapdig Guliyev <tapdigguliyev@gmail.com>
2022-05-07 15:47:58 +0400

    Genre class is now an entity of LibrarianDatabase

--- app/src/main/java/com/raywenderlich/android/librarian/librarian/LibrarianDatabase.kt
+++ app/src/main/java/com/raywenderlich/android/librarian/librarian/LibrarianDatabase.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import androidx.room.*
 import androidx.room.RoomDatabase
 import com.raywenderlich.android.librarian.model.Book
+import com.raywenderlich.android.librarian.model.Genre
 
 const val DATABASE_VERSION = 1
 
 @Database(
-    entities = [Book::class],
+    entities = [Book::class, Genre::class],
     version = DATABASE_VERSION
 )
 
--- app/src/main/java/com/raywenderlich/android/librarian/model/Genre.kt
+++ app/src/main/java/com/raywenderlich/android/librarian/model/Genre.kt
@@ -35,11 +35,15 @@
 package com.raywenderlich.android.librarian.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 @Parcelize
+@Entity
 class Genre(
+    @PrimaryKey
     val id: String = UUID.randomUUID().toString(),
     val name: String
 ) : Parcelable
